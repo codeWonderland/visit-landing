118eb939f9bdc465390dccd0ad42e90a6abe938a
Alice Easter <alice.easter@mymail.champlain.edu>
2018-04-03 16:56:16 -0400

    EOD

--- index.js
+++ index.js
@@ -14,12 +14,6 @@
         sendGaEvent(atag.getAttribute('data-cat'), atag.getAttribute('data-act'), atag.getAttribute('data-label'));
     });
 
-    $('.slide-overlay-image').on('click', function() {
-        var atag = $(this).find('p.slide-point')[0];
-        sendGaEvent(atag.getAttribute('data-cat'), atag.getAttribute('data-act'), atag.getAttribute('data-label'));
-        window.open(atag.getAttribute('data-href'), '_blank');
-    });
-
 }).call(this);
 
 function sendGaEvent(cat, act, label)
